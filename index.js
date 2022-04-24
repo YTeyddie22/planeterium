d1ecf5fedecf9a1811d208603ffcfc041f2a6a0e
Teyddie22 <teyddie@gmail.com>
2022-04-24 11:53:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const habitablePlanet = [];
+const stream = parse({
+	comment: '#',
+	columns: true,
+});
+
+function isHabitable(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
+
+fs.createReadStream('data/kepler_data.csv')
+	.pipe(stream)
+	.on('data', (data) => {
+		if (isHabitable(data)) {
+			habitablePlanet.push(data);
+		}
+	})
+	.on('end', () => {
+		console.log(`${habitablePlanet.length} habitable planets found`);
+	})
+	.on('error', (err) => {
+		console.log(err);
+	});
